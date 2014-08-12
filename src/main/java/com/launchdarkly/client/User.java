a4f61919f290a649559036f4dc9bc03b84fd1e39
John Kodumal <john@catamorphic.com>
2014-08-12 14:14:01 -0700

    More javadocs

--- src/main/java/com/launchdarkly/client/User.java
+++ src/main/java/com/launchdarkly/client/User.java
@@ -1,5 +1,10 @@
 package com.launchdarkly.client;
 
+/**
+ * A {@code User} object contains specific attributes of a user browsing your site. The primary property is their {@code key},
+ * which must uniquely identify each user. For authenticated users, this may be a username or e-mail address. For anonymous users,
+ * this could be an IP address or session ID.
+ */
 public class User {
   private String key;
 
@@ -7,10 +12,18 @@ public class User {
 
   }
 
+  /**
+   * Create a user with the given key
+   * @param key a {@code String} that uniquely identifies a user
+   */
   public User(String key) {
     this.key = key;
   }
 
+  /**
+   * Fetch the key for the user
+   * @return the user's unique key
+   */
   String getKey() {
     return key;
   }
