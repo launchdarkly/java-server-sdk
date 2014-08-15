98e6e6ea384c13028a43e910c2e1abf7c0309798
John Kodumal <john@catamorphic.com>
2014-08-15 11:05:46 -0700

    Builder for LDUser

--- src/main/java/com/launchdarkly/client/LDSampleDriver.java
+++ src/main/java/com/launchdarkly/client/LDSampleDriver.java
@@ -1,5 +1,8 @@
 package com.launchdarkly.client;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * A simple driver illustrating a basic use of the {@link LDClient} library
  */
@@ -8,7 +11,10 @@ public class LDSampleDriver {
   public static void main(String... args) {
     LDClient client = new LDClient("835a5994-d656-4e6e-9175-19780c6ad75f");
 
-    LDUser user = new LDUser("user@test.com");
+    LDUser user = new LDUser.Builder("user@test.com")
+        .country("US")
+        .custom("groups", Arrays.asList("google", "microsoft"))
+        .build();
     boolean flag = client.getFlag("engine.enable", user, false);
 
     System.out.println("Value of flag is " + flag);
