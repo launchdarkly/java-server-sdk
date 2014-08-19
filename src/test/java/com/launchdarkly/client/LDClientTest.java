d3d53d9e1ec0e912aca2d11d6c318e7e63d73e7f
John Kodumal <john@catamorphic.com>
2014-08-19 16:35:46 -0700

    Initial tests for client

---
+++ src/test/java/com/launchdarkly/client/LDClientTest.java
@@ -0,0 +1,28 @@
+package com.launchdarkly.client;
+
+import java.net.URI;
+import java.util.Arrays;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class LDClientTest {
+  LDClient client;
+
+  // TODO create / delete the feature here via API
+
+  @Before
+  public void setupLDClient() {
+    LDConfig config = new LDConfig("7f60f21f-0552-4756-ae32-ca65a0c96ca8", URI.create("http://localhost:8080"));
+    client = new LDClient(config);
+  }
+
+  @Test
+  public void getFlagReturnsTrueForEnabledUser() {
+    LDUser user = new LDUser.Builder("user@test.com")
+        .country("US")
+        .custom("groups", Arrays.asList("google", "microsoft"))
+        .build();
+    boolean flag = client.getFlag("engine.enable", user, false);
+    assertEquals(flag, false);
+  }
+}
