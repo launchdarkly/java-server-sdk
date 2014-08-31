d001c21edc8d54730c7bc8df06dbbb83c6f08078
John Kodumal <john@catamorphic.com>
2014-08-31 10:33:17 -0700

    Feature evaluation for targeting. Support for secondary hash attributes and custom attributes.

--- src/main/java/com/launchdarkly/client/Variation.java
+++ src/main/java/com/launchdarkly/client/Variation.java
@@ -1,24 +1,74 @@
 package com.launchdarkly.client;
 
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 class Variation<E> {
   E value;
   int weight;
   List<String> matches;
-
+  List<TargetRule> targets;
+  private final Logger logger = LoggerFactory.getLogger(Variation.class);
 
   public Variation() {
 
   }
 
-  public boolean matchSegment(LDUser user) {
-    for (String match : matches) {
-      if (user.getKey().equals(match)) {
+  public boolean matchTarget(LDUser user) {
+    for (TargetRule target: targets) {
+      if (matchTarget(user)) {
         return true;
       }
     }
     return false;
+  }
+
+  class TargetRule {
+    String attribute;
+    String operator;
+    List<String> values;
+
+    public boolean matchTarget(LDUser user) {
+      String uValue;
+      if (attribute.equals("key") && user.getKey() != null) {
+        uValue = user.getKey();
+      }
+      else if (attribute.equals("ip") && user.getIp() != null) {
+        uValue = user.getIp();
+      }
+      else if (attribute.equals("country") && user.getCountry() != null) {
+        uValue = user.getCountry();
+      }
+      else { // Custom attribute
+        JsonElement custom = user.getCustom(attribute);
+
+        if (custom != null) {
+          if (custom.isJsonArray()) {
+            JsonArray array = custom.getAsJsonArray();
+            for (JsonElement elt: array) {
+              if (! elt.isJsonPrimitive()) {
+                logger.error("Invalid custom attribute value in user object: " + elt);
+                return false;
+              }
+              else if (values.contains(elt.getAsString())) {
+                return true;
+              }
+            }
+            return false;
+          }
+          else if (custom.isJsonPrimitive()) {
+            return values.contains(custom.getAsString());
+          }
+        }
+        return false;
+      }
+      return values.contains((uValue));
+    }
   }
 }
