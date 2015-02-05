a29aea682fa9496eb7f9eec71a71316afc46fe35
John Kodumal <john@launchdarkly.com>
2015-02-05 10:03:53 -0800

    Change JsonObject -> JsonElement

--- src/main/java/com/launchdarkly/client/CustomEvent.java
+++ src/main/java/com/launchdarkly/client/CustomEvent.java
@@ -1,11 +1,12 @@
 package com.launchdarkly.client;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 class CustomEvent extends Event {
-  private final JsonObject data;
+  private final JsonElement data;
 
-  CustomEvent(String key, LDUser user, JsonObject data) {
+  CustomEvent(String key, LDUser user, JsonElement data) {
     super("custom", key, user);
     this.data = data;
   }
