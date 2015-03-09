003d64793509d88122aabce83e802272f24d4457
John Kodumal <john@launchdarkly.com>
2015-03-09 11:12:38 -0700

    Add support for anonymous, deprecate getFlag in favor of toggle

--- src/test/java/com/launchdarkly/client/FeatureRepTest.java
+++ src/test/java/com/launchdarkly/client/FeatureRepTest.java
@@ -8,17 +8,20 @@ import static org.junit.Assert.*;
 
 public class FeatureRepTest {
 
-  private final Variation.TargetRule targetUserOn = new Variation.TargetRule("key", Collections.singletonList("targetOn@test.com"));
+  private final Variation.TargetRule targetUserOn = new Variation.TargetRule("key", Collections.<Object>singletonList("targetOn@test.com"));
 
-  private final Variation.TargetRule targetGroupOn = new Variation.TargetRule("groups", Arrays.asList("google", "microsoft"));
+  private final Variation.TargetRule targetGroupOn = new Variation.TargetRule("groups", Arrays.<Object>asList("google", "microsoft"));
 
-  private final Variation.TargetRule targetUserOff = new Variation.TargetRule("key", Collections.singletonList("targetOff@test.com"));
+  private final Variation.TargetRule targetUserOff = new Variation.TargetRule("key", Collections.<Object>singletonList("targetOff@test.com"));
 
-  private final Variation.TargetRule targetGroupOff = new Variation.TargetRule("groups", Arrays.asList("oracle"));
+  private final Variation.TargetRule targetGroupOff = new Variation.TargetRule("groups", Arrays.<Object>asList("oracle"));
+
+  private final Variation.TargetRule targetAnonymousOn = new Variation.TargetRule("anonymous", Collections.<Object>singletonList(true));
 
   private final Variation<Boolean> trueVariation = new Variation.Builder<Boolean>(true, 80)
       .target(targetUserOn)
       .target(targetGroupOn)
+      .target(targetAnonymousOn)
       .build();
 
   private final Variation<Boolean> falseVariation = new Variation.Builder<Boolean>(false, 20)
@@ -119,6 +122,14 @@ public class FeatureRepTest {
     Boolean b = simpleFlag.evaluate(user);
 
     assertNotNull(b);
+  }
+
+  @Test
+  public void testFlagWithAnonymousOn() {
+    LDUser user = new LDUser.Builder("targetOff@test.com").anonymous(true).build();
+
+    Boolean b = simpleFlag.evaluate(user);
+    assertEquals(true, b);
   }
 
 }
