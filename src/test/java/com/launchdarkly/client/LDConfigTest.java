fd6bb3bde576e94dc091e6e50b317d9d07cd93fb
Patrick Kaeding <pkaeding@launchdarkly.com>
2015-08-07 14:04:03 -0700

    added ability to pass in HTTP proxy config

--- src/test/java/com/launchdarkly/client/LDConfigTest.java
+++ src/test/java/com/launchdarkly/client/LDConfigTest.java
@@ -1,6 +1,7 @@
 package com.launchdarkly.client;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
@@ -30,5 +31,54 @@ public class LDConfigTest {
     LDConfig config = new LDConfig.Builder().socketTimeoutMillis(3000).build();
 
     assertEquals(3000, config.socketTimeout);
+  }
+
+  @Test
+  public void testNoProxyConfigured() {
+    LDConfig config = new LDConfig.Builder().build();
+
+    assertNull(config.proxyHost);
+  }
+
+  @Test
+  public void testOnlyProxyPortConfiguredHasPort() {
+    LDConfig config = new LDConfig.Builder().proxyPort(1234).build();
+
+    assertEquals(1234, config.proxyHost.getPort());
+  }
+
+  @Test
+  public void testOnlyProxyPortConfiguredHasDefaultHost() {
+    LDConfig config = new LDConfig.Builder().proxyPort(1234).build();
+
+    assertEquals("localhost", config.proxyHost.getHostName());
+  }
+
+  @Test
+  public void testOnlyProxyPortConfiguredHasDefaultScheme() {
+    LDConfig config = new LDConfig.Builder().proxyPort(1234).build();
+
+    assertEquals("https", config.proxyHost.getSchemeName());
+  }
+
+  @Test
+  public void testOnlyProxyHostConfiguredHasDefaultPort() {
+    LDConfig config = new LDConfig.Builder().proxyHost("myproxy.example.com").build();
+
+    assertEquals(-1, config.proxyHost.getPort());
+  }
+
+  @Test
+  public void testProxyHostConfiguredHasHost() {
+    LDConfig config = new LDConfig.Builder().proxyHost("myproxy.example.com").build();
+
+    assertEquals("myproxy.example.com", config.proxyHost.getHostName());
+  }
+
+  @Test
+  public void testProxySchemeConfiguredHasScheme() {
+    LDConfig config = new LDConfig.Builder().proxyScheme("http").build();
+
+    assertEquals("http", config.proxyHost.getSchemeName());
   }
 }
