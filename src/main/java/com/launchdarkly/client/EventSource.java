1f3307400339f39a2500155b9e90a2c3e022ff9b
Patrick Kaeding <pkaeding@launchdarkly.com>
2015-09-04 16:24:02 -0700

    removed extra logging statements

--- src/main/java/com/launchdarkly/client/EventSource.java
+++ src/main/java/com/launchdarkly/client/EventSource.java
@@ -497,7 +497,6 @@ public class EventSource implements EventListener {
       EventInput eventInput = null;
       try {
         try {
-          logger.debug("current state is {}", state.get());
           final Invocation.Builder request = prepareHandshakeRequest();
           if (state.get() == State.OPEN) { // attempt to connect only if even source is open
             logger.debug("Connecting...");
