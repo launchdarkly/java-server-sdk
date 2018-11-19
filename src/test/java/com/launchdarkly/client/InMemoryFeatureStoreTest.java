416c7dd81ee16230ac74a7acd326ec8b400246f6
Eli Bishop <eli@launchdarkly.com>
2018-11-19 14:45:05 -0800

    fix test

--- src/test/java/com/launchdarkly/client/InMemoryFeatureStoreTest.java
+++ src/test/java/com/launchdarkly/client/InMemoryFeatureStoreTest.java
@@ -2,10 +2,6 @@ package com.launchdarkly.client;
 
 public class InMemoryFeatureStoreTest extends FeatureStoreTestBase<InMemoryFeatureStore> {
 
-  public InMemoryFeatureStoreTest(boolean cached) {
-    super(cached);
-  }
-  
   @Override
   protected InMemoryFeatureStore makeStore() {
     return new InMemoryFeatureStore();
