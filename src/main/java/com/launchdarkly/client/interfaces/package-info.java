d00065d1edc768ff64bdac2bcda28eac64003e1e
Eli Bishop <eli@launchdarkly.com>
2020-01-13 21:09:47 -0800

    add package info

---
+++ src/main/java/com/launchdarkly/client/interfaces/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The package for interfaces that allow customization of LaunchDarkly components.
+ * <p>
+ * You will not need to refer to these types in your code unless you are creating a
+ * plug-in component, such as a database integration.
+ */
+package com.launchdarkly.client.interfaces;
