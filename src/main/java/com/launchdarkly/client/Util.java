8a0751d4ad0b8152d0640ca3b3535344c977c984
Eli Bishop <eli@launchdarkly.com>
2020-01-27 16:06:33 -0800

    rm unused

--- src/main/java/com/launchdarkly/client/Util.java
+++ src/main/java/com/launchdarkly/client/Util.java
@@ -51,25 +51,6 @@ class Util {
     }
   }
   
-//  static void configureHttpClientBuilder(LDConfig config, OkHttpClient.Builder builder) {
-//    builder.connectionPool(new ConnectionPool(5, 5, TimeUnit.SECONDS))
-//      .connectTimeout(config.connectTimeout, config.connectTimeoutUnit)
-//      .readTimeout(config.socketTimeout, config.socketTimeoutUnit)
-//      .writeTimeout(config.socketTimeout, config.socketTimeoutUnit)
-//      .retryOnConnectionFailure(false); // we will implement our own retry logic
-//
-//    if (config.sslSocketFactory != null) {
-//      builder.sslSocketFactory(config.sslSocketFactory, config.trustManager);
-//    }
-//
-//    if (config.proxy != null) {
-//      builder.proxy(config.proxy);
-//      if (config.proxyAuthenticator != null) {
-//        builder.proxyAuthenticator(config.proxyAuthenticator);
-//      }
-//    }
-//  }
-  
   static Request.Builder getRequestBuilder(String sdkKey) {
     return new Request.Builder()
         .addHeader("Authorization", sdkKey)
