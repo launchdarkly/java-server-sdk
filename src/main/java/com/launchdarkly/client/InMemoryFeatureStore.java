334a68d7e3540fc8b5ea81dcf4e00479f2d19d85
Eli Bishop <eli@launchdarkly.com>
2020-03-09 13:37:15 -0700

    change in-memory store to use an immutable map and lockless reads

--- src/main/java/com/launchdarkly/client/InMemoryFeatureStore.java
+++ src/main/java/com/launchdarkly/client/InMemoryFeatureStore.java
@@ -1,5 +1,6 @@
 package com.launchdarkly.client;
 
+import com.google.common.collect.ImmutableMap;
 import com.launchdarkly.client.interfaces.DiagnosticDescription;
 import com.launchdarkly.client.value.LDValue;
 
@@ -9,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * A thread-safe, versioned store for feature flags and related data based on a
@@ -18,111 +18,106 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class InMemoryFeatureStore implements FeatureStore, DiagnosticDescription {
   private static final Logger logger = LoggerFactory.getLogger(InMemoryFeatureStore.class);
 
-  private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-  private final Map<VersionedDataKind<?>, Map<String, VersionedData>> allData = new HashMap<>();
+  private volatile ImmutableMap<VersionedDataKind<?>, Map<String, VersionedData>> allData = ImmutableMap.of();
   private volatile boolean initialized = false;
+  private Object writeLock = new Object();
 
   @Override
   public <T extends VersionedData> T get(VersionedDataKind<T> kind, String key) {
-    try {
-      lock.readLock().lock();
-      Map<String, VersionedData> items = allData.get(kind);
-      if (items == null) {
-        logger.debug("[get] no objects exist for \"{}\". Returning null", kind.getNamespace());
-        return null;
-      }
-      Object o = items.get(key);
-      if (o == null) {
-        logger.debug("[get] Key: {} not found in \"{}\". Returning null", key, kind.getNamespace());
-        return null;
-      }
-      if (!kind.getItemClass().isInstance(o)) {
-        logger.warn("[get] Unexpected object class {} found for key: {} in \"{}\". Returning null",
-            o.getClass().getName(), key, kind.getNamespace());
-        return null;
-      }
-      T item = kind.getItemClass().cast(o);
-      if (item.isDeleted()) {
-        logger.debug("[get] Key: {} has been deleted. Returning null", key);
-        return null;
-      }
-      logger.debug("[get] Key: {} with version: {} found in \"{}\".", key, item.getVersion(), kind.getNamespace());
-      return item;
-    } finally {
-      lock.readLock().unlock();
+    Map<String, VersionedData> items = allData.get(kind);
+    if (items == null) {
+      logger.debug("[get] no objects exist for \"{}\". Returning null", kind.getNamespace());
+      return null;
     }
+    Object o = items.get(key);
+    if (o == null) {
+      logger.debug("[get] Key: {} not found in \"{}\". Returning null", key, kind.getNamespace());
+      return null;
+    }
+    if (!kind.getItemClass().isInstance(o)) {
+      logger.warn("[get] Unexpected object class {} found for key: {} in \"{}\". Returning null",
+          o.getClass().getName(), key, kind.getNamespace());
+      return null;
+    }
+    T item = kind.getItemClass().cast(o);
+    if (item.isDeleted()) {
+      logger.debug("[get] Key: {} has been deleted. Returning null", key);
+      return null;
+    }
+    logger.debug("[get] Key: {} with version: {} found in \"{}\".", key, item.getVersion(), kind.getNamespace());
+    return item;
   }
 
   @Override
   public <T extends VersionedData> Map<String, T> all(VersionedDataKind<T> kind) {
-    try {
-      lock.readLock().lock();
-      Map<String, T> fs = new HashMap<>();
-      Map<String, VersionedData> items = allData.get(kind);
-      if (items != null) {
-        for (Map.Entry<String, ? extends VersionedData> entry : items.entrySet()) {
-          if (!entry.getValue().isDeleted()) {
-            fs.put(entry.getKey(), kind.getItemClass().cast(entry.getValue()));
-          }
+    Map<String, T> fs = new HashMap<>();
+    Map<String, VersionedData> items = allData.get(kind);
+    if (items != null) {
+      for (Map.Entry<String, ? extends VersionedData> entry : items.entrySet()) {
+        if (!entry.getValue().isDeleted()) {
+          fs.put(entry.getKey(), kind.getItemClass().cast(entry.getValue()));
         }
       }
-      return fs;
-    } finally {
-      lock.readLock().unlock();
     }
+    return fs;
   }
 
   @Override
   public void init(Map<VersionedDataKind<?>, Map<String, ? extends VersionedData>> allData) {
-    try {
-      lock.writeLock().lock();
-      this.allData.clear();
+    synchronized (writeLock) {
+      ImmutableMap.Builder<VersionedDataKind<?>, Map<String, VersionedData>> newData = ImmutableMap.builder();
       for (Map.Entry<VersionedDataKind<?>, Map<String, ? extends VersionedData>> entry: allData.entrySet()) {
         // Note, the FeatureStore contract specifies that we should clone all of the maps. This doesn't
         // really make a difference in regular use of the SDK, but not doing it could cause unexpected
         // behavior in tests.
-        this.allData.put(entry.getKey(), new HashMap<String, VersionedData>(entry.getValue()));
+        newData.put(entry.getKey(), ImmutableMap.copyOf(entry.getValue()));
       }
-      initialized = true;
-    } finally {
-      lock.writeLock().unlock();
+      this.allData = newData.build(); // replaces the entire map atomically
+      this.initialized = true;
     }
   }
 
   @Override
   public <T extends VersionedData> void delete(VersionedDataKind<T> kind, String key, int version) {
-    try {
-      lock.writeLock().lock();
-      Map<String, VersionedData> items = allData.get(kind);
-      if (items == null) {
-        items = new HashMap<>();
-        allData.put(kind, items);
-      }
-      VersionedData item = items.get(key);
-      if (item == null || item.getVersion() < version) {
-        items.put(key, kind.makeDeletedItem(key, version));
-      }
-    } finally {
-      lock.writeLock().unlock();
-    }
+    upsert(kind, kind.makeDeletedItem(key, version));
   }
 
   @Override
   public <T extends VersionedData> void upsert(VersionedDataKind<T> kind, T item) {
-    try {
-      lock.writeLock().lock();
-      Map<String, VersionedData> items = (Map<String, VersionedData>) allData.get(kind);
-      if (items == null) {
-        items = new HashMap<>();
-        allData.put(kind, items);
+    String key = item.getKey();
+    synchronized (writeLock) {
+      Map<String, VersionedData> existingItems = this.allData.get(kind);
+      VersionedData oldItem = null;
+      if (existingItems != null) {
+        oldItem = existingItems.get(key);
+        if (oldItem.getVersion() >= item.getVersion()) {
+          return;
+        }
       }
-      VersionedData old = items.get(item.getKey());
-
-      if (old == null || old.getVersion() < item.getVersion()) {
-        items.put(item.getKey(), item);
+      // The following logic is necessary because ImmutableMap.Builder doesn't support overwriting an existing key
+      ImmutableMap.Builder<VersionedDataKind<?>, Map<String, VersionedData>> newData = ImmutableMap.builder();
+      for (Map.Entry<VersionedDataKind<?>, Map<String, VersionedData>> e: this.allData.entrySet()) {
+        if (!e.getKey().equals(kind)) {
+          newData.put(e.getKey(), e.getValue());
+        }
       }
-    } finally {
-      lock.writeLock().unlock();
+      if (existingItems == null) {
+        newData.put(kind, ImmutableMap.<String, VersionedData>of(key, item));
+      } else {
+        ImmutableMap.Builder<String, VersionedData> itemsBuilder = ImmutableMap.builder();
+        if (oldItem == null) {
+          itemsBuilder.putAll(existingItems);
+        } else {
+          for (Map.Entry<String, VersionedData> e: existingItems.entrySet()) {
+            if (!e.getKey().equals(key)) {
+              itemsBuilder.put(e.getKey(), e.getValue());
+            }
+          }
+        }
+        itemsBuilder.put(key, item);
+        newData.put(kind, itemsBuilder.build());
+      }
+      this.allData = newData.build(); // replaces the entire map atomically
     }
   }
 
