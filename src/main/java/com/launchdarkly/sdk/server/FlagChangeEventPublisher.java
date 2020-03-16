514d1759ee32e6cb8bf5d2d8317a1ea262216777
Eli Bishop <eli@launchdarkly.com>
2020-03-16 16:48:38 -0700

    use CopyOnWriteArrayList

--- src/main/java/com/launchdarkly/sdk/server/FlagChangeEventPublisher.java
+++ src/main/java/com/launchdarkly/sdk/server/FlagChangeEventPublisher.java
@@ -6,57 +6,34 @@ import com.launchdarkly.sdk.server.interfaces.FlagChangeListener;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 final class FlagChangeEventPublisher implements Closeable {
-  private final List<FlagChangeListener> listeners = new ArrayList<>();
-  private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+  private final CopyOnWriteArrayList<FlagChangeListener> listeners = new CopyOnWriteArrayList<>();
   private volatile ExecutorService executor = null;
   
   public void register(FlagChangeListener listener) {
-    lock.writeLock().lock();
-    try {
-      listeners.add(listener);
+    listeners.add(listener);
+    synchronized (this) {
       if (executor == null) {
         executor = createExecutorService();
       }
-    } finally {
-      lock.writeLock().unlock();
     }
   }
   
   public void unregister(FlagChangeListener listener) {
-    lock.writeLock().lock();
-    try {
-      listeners.remove(listener);
-    } finally {
-      lock.writeLock().unlock();
-    }    
+    listeners.remove(listener);
   }
   
   public boolean hasListeners() {
-    lock.readLock().lock();
-    try {
-      return !listeners.isEmpty();
-    } finally {
-      lock.readLock().unlock();
-    }
+    return !listeners.isEmpty();
   }
   
   public void publishEvent(FlagChangeEvent event) {
-    FlagChangeListener[] ll;
-    lock.readLock().lock();
-    try {
-      ll = listeners.toArray(new FlagChangeListener[listeners.size()]);
-    } finally {
-      lock.readLock().unlock();
-    }
-    for (FlagChangeListener l: ll) {
+    for (FlagChangeListener l: listeners) {
       executor.execute(() -> {
         l.onFlagChange(event);
       });
