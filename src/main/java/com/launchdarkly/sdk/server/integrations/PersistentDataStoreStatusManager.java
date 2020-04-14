8bfffe2867dc7e610a5dd219974b900eb5bd7b10
Eli Bishop <eli@launchdarkly.com>
2020-04-13 17:02:31 -0700

    synchronize again to avoid race condition on creating poller

--- src/main/java/com/launchdarkly/sdk/server/integrations/PersistentDataStoreStatusManager.java
+++ src/main/java/com/launchdarkly/sdk/server/integrations/PersistentDataStoreStatusManager.java
@@ -79,9 +79,11 @@ final class PersistentDataStoreStatusManager {
     // If the store has just become unavailable, start a poller to detect when it comes back. If it has
     // become available, stop any polling we are currently doing.
     if (available) {
-      if (pollerFuture != null) { // don't need to synchronize access here because the state transition was already synchronized above
-        pollerFuture.cancel(false);
-        pollerFuture = null;
+      synchronized (this) {
+        if (pollerFuture != null) {
+          pollerFuture.cancel(false);
+          pollerFuture = null;
+        }
       }
     } else {
       logger.warn("Detected persistent store unavailability; updates will be cached until it recovers");
@@ -98,7 +100,11 @@ final class PersistentDataStoreStatusManager {
           }
         }
       };
-      pollerFuture = scheduler.scheduleAtFixedRate(pollerTask, POLL_INTERVAL_MS, POLL_INTERVAL_MS, TimeUnit.MILLISECONDS);
+      synchronized (this) {
+        if (pollerFuture == null) {
+          pollerFuture = scheduler.scheduleAtFixedRate(pollerTask, POLL_INTERVAL_MS, POLL_INTERVAL_MS, TimeUnit.MILLISECONDS);          
+        }
+      }
     }
   }
   
