e8621a22cd87855f21fb095b57e81d56e3910ab4
Eli Bishop <eli@launchdarkly.com>
2020-05-13 18:38:33 -0700

    typo

--- src/main/java/com/launchdarkly/sdk/server/interfaces/package-info.java
+++ src/main/java/com/launchdarkly/sdk/server/interfaces/package-info.java
@@ -2,7 +2,7 @@
  * The package for interfaces that allow customization of LaunchDarkly components, and interfaces
  * to other advanced SDK features. 
  * <p>
- * Most application will not need to refer to these types. You will use them if you are creating a
+ * Most applications will not need to refer to these types. You will use them if you are creating a
  * plug-in component, such as a database integration, or if you use advanced features such as
  * {@link com.launchdarkly.sdk.server.LDClientInterface#getDataStoreStatusProvider()} or
  * {@link com.launchdarkly.sdk.server.LDClientInterface#registerFlagChangeListener(FlagChangeListener)}.
