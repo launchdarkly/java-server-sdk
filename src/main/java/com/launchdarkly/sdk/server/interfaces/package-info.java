223e0385d3b8f8e56e80d593cc568ea534557246
Eli Bishop <eli@launchdarkly.com>
2020-05-20 14:35:49 -0700

    fix merge conflict

--- src/main/java/com/launchdarkly/sdk/server/interfaces/package-info.java
+++ src/main/java/com/launchdarkly/sdk/server/interfaces/package-info.java
@@ -4,12 +4,7 @@
  * <p>
  * Most applications will not need to refer to these types. You will use them if you are creating a
  * plug-in component, such as a database integration, or if you use advanced features such as
-<<<<<<< HEAD
- * {@link com.launchdarkly.sdk.server.LDClientInterface#getDataStoreStatusProvider()} or
- * {@link com.launchdarkly.sdk.server.LDClientInterface#getFlagTracker()}.
-=======
  * {@link com.launchdarkly.sdk.server.interfaces.LDClientInterface#getDataStoreStatusProvider()} or
- * {@link com.launchdarkly.sdk.server.interfaces.LDClientInterface#registerFlagChangeListener(FlagChangeListener)}.
->>>>>>> 5.x
+ * {@link com.launchdarkly.sdk.server.interfaces.LDClientInterface#getFlagTracker()}.
  */
 package com.launchdarkly.sdk.server.interfaces;
