22eb63eac172384c6ee342746ca040720c46bbba
LaunchDarklyReleaseBot <launchdarklyreleasebot@launchdarkly.com>
2022-07-01 00:40:24 +0000

    Releasing version 5.9.1

--- src/main/java/com/launchdarkly/sdk/server/Version.java
+++ src/main/java/com/launchdarkly/sdk/server/Version.java
@@ -4,5 +4,5 @@ abstract class Version {
   private Version() {}
   
   // This constant is updated automatically by our Gradle script during a release, if the project version has changed
-  static final String SDK_VERSION = "5.9.0";
+  static final String SDK_VERSION = "5.9.1";
 }
