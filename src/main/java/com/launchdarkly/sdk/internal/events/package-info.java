fbeb610a3544c28bd929e87170b1257556065ccc
Eli Bishop <eli@launchdarkly.com>
2022-07-29 18:45:02 -0700

    javadocs

---
+++ src/main/java/com/launchdarkly/sdk/internal/events/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the core implementation of analytics events and diagnostic events
+ * used by the Java SDK and Android SDK.
+ * <p>
+ * All types in this package are for internal LaunchDarkly use only, and are subject to change.
+ * They are not part of the public supported API of the SDKs, and they should not be referenced
+ * by application code.
+ */
+package com.launchdarkly.sdk.internal.events;
